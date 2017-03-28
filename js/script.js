5d50969fa84c54fac92b3375cef6f4d24bfa390c
dolonche <ivan.pitenin@gmail.com>
2017-03-28 14:55:28 +0300

    small fix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
     var navMain = document.querySelector('.main-nav');
     var navToggle = document.querySelector('.main-nav__toggle');
-    var popUp = document.querySelector('.popup');
+    var popup = document.querySelector('.popup');
     var cart = document.querySelector('.product__cart');
     var productWeek = document.querySelector('.product-week__btn')
-    var overLay = document.querySelector('.overlay');
+    var overlay = document.querySelector('.overlay');
     navMain.classList.remove('main-nav--no-js');
 
     navToggle.addEventListener('click', function () {
@@ -16,30 +16,30 @@
       }
     });
     cart.addEventListener('click', function () {
-      if (popUp.classList.contains('popup--open')) {
-        popUp.classList.remove('popup--open');
+      if (popup.classList.contains('popup--open')) {
+        popup.classList.remove('popup--open');
       } else {
-        popUp.classList.add('popup--open');
+        popup.classList.add('popup--open');
       }
     });
     cart.addEventListener('click', function () {
-      if (overLay.classList.contains('overlay--open')) {
-        overLay.classList.remove('overlay--open');
+      if (overlay.classList.contains('overlay--open')) {
+        overlay.classList.remove('overlay--open');
       } else {
-        overLay.classList.add('overlay--open');
+        ovelay.classList.add('overlay--open');
       }
     });
     productWeek.addEventListener('click', function () {
-      if (popUp.classList.contains('popup--open')) {
-        popUp.classList.remove('popup--open');
+      if (popup.classList.contains('popup--open')) {
+        popup.classList.remove('popup--open');
       } else {
-        popUp.classList.add('popup--open');
+        popup.classList.add('popup--open');
       }
     });
     productWeek.addEventListener('click', function () {
-      if (overLay.classList.contains('overlay--open')) {
-        overLay.classList.remove('overlay--open');
+      if (overlay.classList.contains('overlay--open')) {
+        overlay.classList.remove('overlay--open');
       } else {
-        overLay.classList.add('overlay--open');
+        overlay.classList.add('overlay--open');
       }
     });
